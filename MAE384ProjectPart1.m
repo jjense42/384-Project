eacec8834de63334e48e3d3797082dddd93a98e4
TylerCain1 <twcain@asu.edu>
2024-11-25 12:27:53 -0700

    Update MAE384ProjectPart1.m
    Added (t,1) to each K thingamobob

--- MAE384ProjectPart1.m
+++ MAE384ProjectPart1.m
@@ -1,5 +1,8 @@
 %Runge-Kutta Method
-% i can edit code here
+format shortG
+clear
+clc
+
 St(1,1) = 990; %Number of susceptible individuals at time t
 It(1,1) = 10; %Number of infected individuals at time t
 Rt(1,1) = 0; %Number of recovered individuals at time t
@@ -15,25 +18,26 @@ dRdt = @(t, St, It, Rt) Gamma .* It;
 
 for t = 1:h:T
     
-    k1S = dSdt(t, St, It, Rt);
-    k1I = dIdt(t, St, It, Rt);
-    k1R = dRdt(t, St, It, Rt);
+    k1S = dSdt(t, St(t,1), It(t,1), Rt(t,1));
+    k1I = dIdt(t, St(t,1), It(t,1), Rt(t,1));
+    k1R = dRdt(t, St(t,1), It(t,1), Rt(t,1));
 
-    k2S = dSdt(t + 0.5 * h, St + 0.5 * k1S * h, It + 0.5 * k1I * h, Rt + 0.5 * k1R * h);
-    k2I = dIdt(t + 0.5 * h, St + 0.5 * k1S * h, It + 0.5 * k1I * h, Rt + 0.5 * k1R * h);
-    k2R = dRdt(t + 0.5 * h, St + 0.5 * k1S * h, It + 0.5 * k1I * h, Rt + 0.5 * k1R * h);
+    k2S = dSdt(t + 0.5 * h, St(t,1) + 0.5 * k1S * h, It(t,1) + 0.5 * k1I * h, Rt(t,1) + 0.5 * k1R * h);
+    k2I = dIdt(t + 0.5 * h, St(t,1) + 0.5 * k1S * h, It(t,1) + 0.5 * k1I * h, Rt(t,1) + 0.5 * k1R * h);
+    k2R = dRdt(t + 0.5 * h, St(t,1) + 0.5 * k1S * h, It(t,1) + 0.5 * k1I * h, Rt(t,1) + 0.5 * k1R * h);
 
-    k3S = dSdt(t + 0.5 * h, St + 0.5 * k2S * h, It + 0.5 * k2I * h, Rt + 0.5 * k2R * h);
-    k3I = dIdt(t + 0.5 * h, St + 0.5 * k2S * h, It + 0.5 * k2I * h, Rt + 0.5 * k2R * h);
-    k3R = dRdt(t + 0.5 * h, St + 0.5 * k2S * h, It + 0.5 * k2I * h, Rt + 0.5 * k2R * h);
+    k3S = dSdt(t + 0.5 * h, St(t,1) + 0.5 * k2S * h, It(t,1) + 0.5 * k2I * h, Rt(t,1) + 0.5 * k2R * h);
+    k3I = dIdt(t + 0.5 * h, St(t,1) + 0.5 * k2S * h, It(t,1) + 0.5 * k2I * h, Rt(t,1) + 0.5 * k2R * h);
+    k3R = dRdt(t + 0.5 * h, St(t,1) + 0.5 * k2S * h, It(t,1) + 0.5 * k2I * h, Rt(t,1) + 0.5 * k2R * h);
 
-    k4S = dSdt(t + h, St + k3S * h, It + k3I, Rt + k3R);
-    k4I = dIdt(t + h, St + k3S * h, It + k3I, Rt + k3R);
-    k4R = dRdt(t + h, St + k3S * h, It + k3I, Rt + k3R);
+    k4S = dSdt(t + h, St(t,1) + k3S * h, It(t,1) + k3I, Rt(t,1) + k3R);
+    k4I = dIdt(t + h, St(t,1) + k3S * h, It(t,1) + k3I, Rt(t,1) + k3R);
+    k4R = dRdt(t + h, St(t,1) + k3S * h, It(t,1) + k3I, Rt(t,1) + k3R);
 
 
     St(t + 1,1) = St(t,1) + (1/6) * (k1S + 2 * k2S + 2 * k3S + k4S) * h;
-    It(t + 1,1) = It(t,1) + (1/6) * (k1I + 2 * k2I + 2 * k3I + k4I) * h;
-    Rt(t + 1,1) = Rt(t,1) + (1/6) * (k1R + 2 * k2R + 2 * k3R + k4R) * h;
+    %It(t + 1,1) = It(t,1) + (1/6) * (k1I + 2 * k2I + 2 * k3I + k4I) * h;
+    %Rt(t + 1,1) = Rt(t,1) + (1/6) * (k1R + 2 * k2R + 2 * k3R + k4R) * h;
+
 
 end
