45daf7941c2ee38c59696a3cf661125de3128fe1
TylerCain1 <twcain@asu.edu>
2024-11-25 12:47:48 -0700

    Update MAE384ProjectPart1.m
    Added loops for different Beta and Gamma values

--- MAE384ProjectPart1.m
+++ MAE384ProjectPart1.m
@@ -3,9 +3,20 @@ format shortG
 clear
 clc
 
+%Variable Definitions
+St1 = zeros(1,1);
+It1 = zeros(1,1);
+Rt1 = zeros(1,1);
+St2 = zeros(1,1);
+It2 = zeros(1,1);
+Rt2 = zeros(1,1);
+St3 = zeros(1,1);
+It3 = zeros(1,1);
+Rt3 = zeros(1,1);
 St(1,1) = 990; %Number of susceptible individuals at time t
 It(1,1) = 10; %Number of infected individuals at time t
 Rt(1,1) = 0; %Number of recovered individuals at time t
+
 N = St + It + Rt; %Total Population
 Beta = 0.3; %Transmission Rate
 Gamma = 0.1; %Recovery Rate
@@ -39,5 +50,72 @@ for t = 1:h:T
     It(t + 1,1) = It(t,1) + (1/6) * (k1I + 2 * k2I + 2 * k3I + k4I) * h;
     Rt(t + 1,1) = Rt(t,1) + (1/6) * (k1R + 2 * k2R + 2 * k3R + k4R) * h;
 
+    St1(t,1) = St(t,1);
+    It1(t,1) = It(t,1);
+    Rt1(t,1) = Rt(t,1);
+
+end
+
+for t = 1:h:T
+
+    Beta = 1;
+    Gamma = 0.1;
+    
+    k1S = dSdt(t, St(t,1), It(t,1), Rt(t,1));
+    k1I = dIdt(t, St(t,1), It(t,1), Rt(t,1));
+    k1R = dRdt(t, St(t,1), It(t,1), Rt(t,1));
+
+    k2S = dSdt(t + 0.5 * h, St(t,1) + 0.5 * k1S * h, It(t,1) + 0.5 * k1I * h, Rt(t,1) + 0.5 * k1R * h);
+    k2I = dIdt(t + 0.5 * h, St(t,1) + 0.5 * k1S * h, It(t,1) + 0.5 * k1I * h, Rt(t,1) + 0.5 * k1R * h);
+    k2R = dRdt(t + 0.5 * h, St(t,1) + 0.5 * k1S * h, It(t,1) + 0.5 * k1I * h, Rt(t,1) + 0.5 * k1R * h);
+
+    k3S = dSdt(t + 0.5 * h, St(t,1) + 0.5 * k2S * h, It(t,1) + 0.5 * k2I * h, Rt(t,1) + 0.5 * k2R * h);
+    k3I = dIdt(t + 0.5 * h, St(t,1) + 0.5 * k2S * h, It(t,1) + 0.5 * k2I * h, Rt(t,1) + 0.5 * k2R * h);
+    k3R = dRdt(t + 0.5 * h, St(t,1) + 0.5 * k2S * h, It(t,1) + 0.5 * k2I * h, Rt(t,1) + 0.5 * k2R * h);
+
+    k4S = dSdt(t + h, St(t,1) + k3S * h, It(t,1) + k3I, Rt(t,1) + k3R);
+    k4I = dIdt(t + h, St(t,1) + k3S * h, It(t,1) + k3I, Rt(t,1) + k3R);
+    k4R = dRdt(t + h, St(t,1) + k3S * h, It(t,1) + k3I, Rt(t,1) + k3R);
+
+
+    St(t + 1,1) = St(t,1) + (1/6) * (k1S + 2 * k2S + 2 * k3S + k4S) * h;
+    It(t + 1,1) = It(t,1) + (1/6) * (k1I + 2 * k2I + 2 * k3I + k4I) * h;
+    Rt(t + 1,1) = Rt(t,1) + (1/6) * (k1R + 2 * k2R + 2 * k3R + k4R) * h;
+
+    St2(t,1) = St(t,1);
+    It2(t,1) = It(t,1);
+    Rt2(t,1) = Rt(t,1);
+
+end
+
+for t = 1:h:T
+
+    Beta = 2;
+    Gamma = 0.2;
+    
+    k1S = dSdt(t, St(t,1), It(t,1), Rt(t,1));
+    k1I = dIdt(t, St(t,1), It(t,1), Rt(t,1));
+    k1R = dRdt(t, St(t,1), It(t,1), Rt(t,1));
+
+    k2S = dSdt(t + 0.5 * h, St(t,1) + 0.5 * k1S * h, It(t,1) + 0.5 * k1I * h, Rt(t,1) + 0.5 * k1R * h);
+    k2I = dIdt(t + 0.5 * h, St(t,1) + 0.5 * k1S * h, It(t,1) + 0.5 * k1I * h, Rt(t,1) + 0.5 * k1R * h);
+    k2R = dRdt(t + 0.5 * h, St(t,1) + 0.5 * k1S * h, It(t,1) + 0.5 * k1I * h, Rt(t,1) + 0.5 * k1R * h);
+
+    k3S = dSdt(t + 0.5 * h, St(t,1) + 0.5 * k2S * h, It(t,1) + 0.5 * k2I * h, Rt(t,1) + 0.5 * k2R * h);
+    k3I = dIdt(t + 0.5 * h, St(t,1) + 0.5 * k2S * h, It(t,1) + 0.5 * k2I * h, Rt(t,1) + 0.5 * k2R * h);
+    k3R = dRdt(t + 0.5 * h, St(t,1) + 0.5 * k2S * h, It(t,1) + 0.5 * k2I * h, Rt(t,1) + 0.5 * k2R * h);
+
+    k4S = dSdt(t + h, St(t,1) + k3S * h, It(t,1) + k3I, Rt(t,1) + k3R);
+    k4I = dIdt(t + h, St(t,1) + k3S * h, It(t,1) + k3I, Rt(t,1) + k3R);
+    k4R = dRdt(t + h, St(t,1) + k3S * h, It(t,1) + k3I, Rt(t,1) + k3R);
+
+
+    St(t + 1,1) = St(t,1) + (1/6) * (k1S + 2 * k2S + 2 * k3S + k4S) * h;
+    It(t + 1,1) = It(t,1) + (1/6) * (k1I + 2 * k2I + 2 * k3I + k4I) * h;
+    Rt(t + 1,1) = Rt(t,1) + (1/6) * (k1R + 2 * k2R + 2 * k3R + k4R) * h;
+
+    St3(t,1) = St(t,1);
+    It3(t,1) = It(t,1);
+    Rt3(t,1) = Rt(t,1);
 
 end
