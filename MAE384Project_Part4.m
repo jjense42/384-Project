fdead15598dc68bf48adf8550fde968df177d243
KJ-TheFlexy <kajohn86@asu.edu>
2024-11-27 16:26:19 -0700

    Update MAE384Project_Part4.m
    Adjusted a few comments, adjusted figures, and included a discussion section.

--- MAE384Project_Part4.m
+++ MAE384Project_Part4.m
@@ -6,32 +6,29 @@ clc
 St1 = zeros(1,1);
 It1 = zeros(1,1);
 Rt1 = zeros(1,1);
-St2 = zeros(1,1);
-It2 = zeros(1,1);
-Rt2 = zeros(1,1);
 
-A = 5; %Amplitude
-w1 = 2 * pi * 365/365; %Angular Frequency
+A = 5; % Amplitude
+w1 = 2 * pi * 365/365; % Angular Frequency
 w2 = 2 * pi * 100/365; % Angular Frequency for #6
-Beta0 = 0.3; %Transmission Rate IC
-Gamma = 0.1; %Recovery Rate
-St(1,1) = 990; %Number of susceptible individuals at time t
-It(1,1) = 10; %Number of infected individuals at time t
-Rt(1,1) = 0; %Number of recovered individuals at time t
+Beta0 = 0.3; % Transmission Rate IC
+Gamma = 0.1; % Recovery Rate
+St(1,1) = 990; % Number of susceptible individuals at time t
+It(1,1) = 10; % Number of infected individuals at time t
+Rt(1,1) = 0; % Number of recovered individuals at time t
 
-N = St + It + Rt; %Total Population
+N = St + It + Rt; % Total Population
 
-h = 0.1; %Step size in days
-T1 = 30; %Total simulation time in days (0 -> 30)
+h = 0.1; % Step size in days
+T1 = 30; % Total simulation time in days (0 -> 30)
 T2 = 30; % Total simulation time in days for #6
 
 days = 1:h:T1;
 f1 = (1/T1)*(0:length(days)-1); % frequncy vector used for plotting
 days2 = (1:h:T2); 
 f2 = (1/T1)*(0:length(days)-1); % frequency vector for #6
-t1 = 1; %Counting variable
+t1 = 1; % Counting variable
 
-for t = 1:h:T1 % For loop for #5
+for t = 1:h:T1 % Step 5
 
     Beta = Beta0 * (1 + A * sin(w1 * t));
 
@@ -68,7 +65,6 @@ for t = 1:h:T1 % For loop for #5
 
 end
 
-
 figure(1)
 hold on
 grid on
@@ -77,14 +73,12 @@ plot(days,St1,'red','LineWidth',2)
 plot(days,It1,'green','LineWidth',2)
 plot(days,Rt1,'blue','linewidth',2)
 
-title('Seasonal Influenza, ß = 0.3(1 + 5sin(wt) y = 0.1')
+title('Seasonal Influenza, w = 2*pi*365/365')
 xlabel('Time in Days')
 ylabel('People')
 legend('Susceptible','Infected','Recovered')
 
 hold off
-
-%Q 3 has a written qustion that needs to be answered. 
 
 fftSt1 = abs(fft(St1));
 fftIt1 = abs(fft(It1));
@@ -97,18 +91,17 @@ figure(2)
 hold on
 grid on
 
-plot(f1,fftIt1,'b','LineWidth',2);
+plot(f1,fftIt1,'g','LineWidth',2);
 title('Fourier Analysis of Infected Cases')
 legend('Discrete Fourier Transform')
-ylabel('FFT')
-xlabel('f')
+ylabel('Amplitude')
+xlabel('frequency')
 axis([0 5, 0 m])
 
 hold off
 
 t1 = 1;
-
-for t = 1:h:T1 % For loop for #6
+for t = 1:h:T1 % Step 6
 
     Beta = Beta0 * (1 + A * sin(w2 * t));
 
@@ -145,19 +138,50 @@ for t = 1:h:T1 % For loop for #6
 
 end
 
+figure(3)
+hold on
+grid on
+
+plot(days,St2,'red','LineWidth',2)
+plot(days,It2,'green','LineWidth',2)
+plot(days,Rt2,'blue','linewidth',2)
+
+title('Seasonal Influenza, w = 2*pi*100/365')
+xlabel('Time in Days')
+ylabel('People')
+legend('Susceptible','Infected','Recovered')
+
+hold off
+
 fftSt2 = abs(fft(St2));
 fftIt2 = abs(fft(It2));
 fftRt2 = abs(fft(Rt2));
 
 m2 = max(fftIt2);
 
-figure(3)
+figure(4)
 hold on
 grid on
 
-plot(f2,fftIt2,'b','LineWidth',2);
+plot(f2,fftIt2,'g','LineWidth',2);
 title('Fourier Analysis of Infected Cases')
 legend('Discrete Fourier Transform')
-ylabel('FFT')
-xlabel('f')
+ylabel('Amplitude')
+xlabel('frequency')
 axis([0 5, 0 m2])
+
+%% Discussion Section
+% Step 3: Yes, we see that the sinusoidal nature of
+% the function causes the susceptible and infected graphs to oscillatate at
+% an increased rate. It will reach the local minimums and maximums more
+% frequently as we increase the frequency. 
+
+% Step 5: This does make physical sense. Lower frequency 
+% corresponds to 1 day while the higher frequency corresponds to a period 
+% of 3 days. When people congregate less frenquently, they gether in larger
+% groups, which spikes the transmission rate.
+
+% Step 6: A lower value of angular frequency implies that the peaks of 
+% infected and susceptible people occured less frequently but at a higher 
+% amplitude to preserve the frequency. The magnitude of the frequency 
+% shifted to a lower value, but the amplitude shifted to a higher value.
