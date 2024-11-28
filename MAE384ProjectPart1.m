ad2b288745c56fb7e8303431c6861c622284dfcf
KJ-TheFlexy <kajohn86@asu.edu>
2024-11-27 17:05:37 -0700

    Update MAE384ProjectPart1.m
    Added several comments to make code more readable

--- MAE384ProjectPart1.m
+++ MAE384ProjectPart1.m
@@ -1,9 +1,7 @@
-%Runge-Kutta Method
-format shortG
-clear
-clc
+%% Part 1: Runge-Kutta Method
+format shortG; clear; clc
 
-%Variable Definitions
+% Initializing Variable Definitions
 St1 = zeros(1,1);
 It1 = zeros(1,1);
 Rt1 = zeros(1,1);
@@ -13,34 +11,27 @@ Rt2 = zeros(1,1);
 St3 = zeros(1,1);
 It3 = zeros(1,1);
 Rt3 = zeros(1,1);
-St(1,1) = 990; %Number of susceptible individuals at time t
-It(1,1) = 10; %Number of infected individuals at time t
-Rt(1,1) = 0; %Number of recovered individuals at time t
+St(1,1) = 990; % Number of susceptible individuals at time t
+It(1,1) = 10; % Number of infected individuals at time t
+Rt(1,1) = 0; % Number of recovered individuals at time t
 
-N = St + It + Rt; %Total Population
-
-h = 1; %Step size in days
-T = 100; %Total simulation time in days (0 -> 100)
-
-
-
+N = St + It + Rt; % Total Population
+h = 1; % Step size in days
+T = 100; % Total simulation time in days (0 -> 100)
 days = 1:h:100;
 
 for t = 1:h:T % For loop calculation for the standard flu 
 
-% variables
-
+    % initially given variables
     Beta = 0.3; %Transmission Rate
     Gamma = 0.1; %Recovery Rate
 
-% Set of nonlinear differential equations
-
+    % Set of nonlinear differential equations defined
     dSdt = @(t, St, It, Rt) -(Beta / N) .* St .* It;
     dIdt = @(t, St, It, Rt) (Beta / N) .* St .* It - Gamma .* It;
     dRdt = @(t, St, It, Rt) Gamma .* It;
 
-% Runge Kutta Calculations. Each diff eq gets evaluated
-    
+    % Runge Kutta Calculations in which each diff eq is evaluated  
     k1S = dSdt(t, St(t,1), It(t,1), Rt(t,1));
     k1I = dIdt(t, St(t,1), It(t,1), Rt(t,1));
     k1R = dRdt(t, St(t,1), It(t,1), Rt(t,1));
@@ -62,13 +53,14 @@ for t = 1:h:T % For loop calculation for the standard flu
     It(t + 1,1) = It(t,1) + (1/6) * (k1I + 2 * k2I + 2 * k3I + k4I) * h;
     Rt(t + 1,1) = Rt(t,1) + (1/6) * (k1R + 2 * k2R + 2 * k3R + k4R) * h;
 
-% Values for susceptible, infected, and recovered persons is retabulated into a vector to be plotted later
+    % Values for susceptible, infected, and recovered persons are tabulated and synthesized into a vector
 
     St1(t,1) = St(t,1);
     It1(t,1) = It(t,1);
     Rt1(t,1) = Rt(t,1);
 
 end
+
 
 for t = 1:h:T
 
@@ -106,15 +98,19 @@ for t = 1:h:T
 
 end
 
+% Begin 
 for t = 1:h:T
 
+    % Re-initialize givens
     Beta = 2;
     Gamma = 0.2;
     
+    % Next set of nonlinear differential equations defined
     dSdt = @(t, St, It, Rt) -(Beta / N) .* St .* It;
     dIdt = @(t, St, It, Rt) (Beta / N) .* St .* It - Gamma .* It;
     dRdt = @(t, St, It, Rt) Gamma .* It;
-
+    
+    % Define all constants for the RK method, for each model
     k1S = dSdt(t, St(t,1), It(t,1), Rt(t,1));
     k1I = dIdt(t, St(t,1), It(t,1), Rt(t,1));
     k1R = dRdt(t, St(t,1), It(t,1), Rt(t,1));
@@ -131,7 +127,7 @@ for t = 1:h:T
     k4I = dIdt(t + h, St(t,1) + k3S * h, It(t,1) + k3I, Rt(t,1) + k3R);
     k4R = dRdt(t + h, St(t,1) + k3S * h, It(t,1) + k3I, Rt(t,1) + k3R);
 
-
+    % RK method, employing constants created ealrier
     St(t + 1,1) = St(t,1) + (1/6) * (k1S + 2 * k2S + 2 * k3S + k4S) * h;
     It(t + 1,1) = It(t,1) + (1/6) * (k1I + 2 * k2I + 2 * k3I + k4I) * h;
     Rt(t + 1,1) = Rt(t,1) + (1/6) * (k1R + 2 * k2R + 2 * k3R + k4R) * h;
@@ -143,8 +139,7 @@ for t = 1:h:T
 end
 
 
-
-figure(1) % Figure for Seasonal Influenza
+figure(1) % Plot Seasonal Influenza
 hold on
 grid on
 
@@ -156,10 +151,10 @@ title('Seasonal Influenza, ß = 0.3 y = 0.1')
 xlabel('Time in Days')
 ylabel('People')
 legend('Susceptible','Infected','Recovered')
-
 hold off
 
-figure(2) Figure for Covid-19
+
+figure(2) % Plot COVID-19
 hold on
 grid on
 
@@ -171,10 +166,10 @@ title('COVID-19, ß = 1 y = 0.1')
 xlabel('Time in Days')
 ylabel('People')
 legend('Susceptible','Infected','Recovered')
-
 hold off
 
-figure(3) Figure for Measels
+
+figure(3) % Plot Measles
 hold on
 grid on
 
@@ -186,16 +181,18 @@ title('Measles, ß = 2 y = .2')
 xlabel('Time in Days')
 ylabel('People')
 legend('Susceptible','Infected','Recovered')
-
 hold off
 
+
+%% Discussion Section:
+
 % The beta value is the transmission rate, or how often susceptible
-% individuals get infected while gamma is recovery rate, or how often the
+% individuals get infected while gamma is the recovery rate, or how often the
 % affected individuals recover. When the beta parameter increases, the
 % number of susceptible individuals decreases significantly quicker, the
-% overall recovery rate is signifcantly quicker, and the infected
-% population spikes to a greater amplitude ealier. When the gamma parameter
+% overall recovery rate is considerably quicker, and the infected
+% population spikes to a greater amplitude earlier. When the gamma parameter
 % doubles along with the beta parameter, the susceptible, infected, and
 % recovered populations all have similar behavior, except they all exhibit
-% the end behavior twice as quickly. the dervatives are much sharper
-% (change in the populations occur much quicker). 
+% the end behavior twice as quickly. the derivatives are much sharper
+% (change in the populations occurs much quicker). 
