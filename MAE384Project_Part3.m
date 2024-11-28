399f07d342db661e58c0045d5a091e9b0fcd7fe3
KJ-TheFlexy <kajohn86@asu.edu>
2024-11-27 17:15:41 -0700

    Update MAE384Project_Part3.m
    Clarifying Comments

--- MAE384Project_Part3.m
+++ MAE384Project_Part3.m
@@ -1,34 +1,32 @@
-format shortG
-clear
-clc
+format shortG; clear; clc
 
-%Variable Definitions
+% Initializing Given Variables
 St1 = zeros(1,1);
 It1 = zeros(1,1);
 Rt1 = zeros(1,1);
 
-St(1,1) = 990; %Number of susceptible individuals at time t
-It(1,1) = 10; %Number of infected individuals at time t
-Rt(1,1) = 0; %Number of recovered individuals at time t
+St(1,1) = 990; % Number of susceptible individuals at time t
+It(1,1) = 10; % Number of infected individuals at time t
+Rt(1,1) = 0; % Number of recovered individuals at time t
 
-N = St + It + Rt; %Total Population
-
-h = 1; %Step size in days
-T = 30; %Total simulation time in days (0 -> 100)
-
-
-
+N = St + It + Rt; % Total Population
+h = 1; % Step size in days
+T = 30; % Total simulation time in days (0 -> 100)
 days = 1:h:100;
 
-for t = 1:h:T
 
+for t = 1:h:T
+    
+    % Defining additional given values
     Beta = 0.3; %Transmission Rate
     Gamma = 0.1; %Recovery Rate
 
+    % Defining diff eqs for Susceptible, Infected, and Recovered populations
     dSdt = @(t, St, It, Rt) -(Beta / N) .* St .* It;
     dIdt = @(t, St, It, Rt) (Beta / N) .* St .* It - Gamma .* It;
     dRdt = @(t, St, It, Rt) Gamma .* It;
-    
+
+    % Defining all needed constants
     k1S = dSdt(t, St(t,1), It(t,1), Rt(t,1));
     k1I = dIdt(t, St(t,1), It(t,1), Rt(t,1));
     k1R = dRdt(t, St(t,1), It(t,1), Rt(t,1));
@@ -45,7 +43,7 @@ for t = 1:h:T
     k4I = dIdt(t + h, St(t,1) + k3S * h, It(t,1) + k3I, Rt(t,1) + k3R);
     k4R = dRdt(t + h, St(t,1) + k3S * h, It(t,1) + k3I, Rt(t,1) + k3R);
 
-
+    % Approximating, using all constants produced previously
     St(t + 1,1) = St(t,1) + (1/6) * (k1S + 2 * k2S + 2 * k3S + k4S) * h;
     It(t + 1,1) = It(t,1) + (1/6) * (k1I + 2 * k2I + 2 * k3I + k4I) * h;
     Rt(t + 1,1) = Rt(t,1) + (1/6) * (k1R + 2 * k2R + 2 * k3R + k4R) * h;
@@ -56,31 +54,29 @@ for t = 1:h:T
 
 end
 
-%linear least squares with T = 30
+% linear least squares with T = 30
 T = 30;
 x=days(1:T);
 y=log((It1(1:T))');
 
-
-%solve for ln I(t)=ln I(0)+kt. A1=k A0=ln I(0)
+% Solve for ln I(t)=ln I(0)+kt. A1=k A0=ln I(0)
 A1= (T*sum(x.*y)-(sum(x)*sum(y)))/(T*sum(x.^2)-(sum(x)^2));
 A0= (sum(y)/T)-((A1/T)*(sum(x)));
 I0=exp(A0);
 b=(A1+Gamma)*(N/St(1,1));
 
-
-
-%linear least squares with T = 10
+% Linear least squares with T = 10
 T = 10;
 x1=days(1:T);
 y1=log((It1(1:T))');
 
-%solve for ln I(t)=ln I(0)+kt. A11=k A01=ln I(0)
+% Solving for ln I(t)=ln I(0)+kt. A11=k A01=ln I(0)
 A11= (T*sum(x1.*y1)-(sum(x1)*sum(y1)))/(T*sum(x1.^2)-((sum(x1))^2));
 A01= ((sum(y1))/T)-(A11*(sum(x1)/T));
 I01=exp(A01);
 b1=(A11+Gamma)*(N/St(1,1));
 
+% Display all results
 disp('Estimimated ß for T = 30:');
 disp(b);
 disp('Estimimated I(O) for T = 30:');
@@ -93,7 +89,7 @@ disp(I01);
 %% Discussion Section
 
 % As you decrease the T value (number of days), the estimated values for 
-% I(0) and beta converge to the true values. Decreasing the number of data 
-% points, the I(t) curve becomes more like an exponential. Therefore, 
-% the exponential approximation will more accurately predict the actual
-% data curve. 
+% I(0) and beta converge to the true values. Reducing the number of data 
+% points, the I(t) curve approaches an exponential model within those boundaries.
+% Therefore, the exponential approximation will  predict the actual data points 
+% more accurately. 
